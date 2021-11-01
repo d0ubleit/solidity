a1a09d5284c34287ac672cfbac8117cc2813944e
d0ubleit <92369748+d0ubleit@users.noreply.github.com>
2021-11-01 15:21:12 +0300

    Unit contracts had same address. Fixed

--- 06_warGame/WarGameWarrior.sol
+++ 06_warGame/WarGameWarrior.sol
@@ -3,8 +3,14 @@ pragma AbiHeader expire;
 import "WarGameUnit.sol";
 
 contract WarGameWarrior is WarGameUnit {
+
+    string UnitName = "Warrior";
     
-    constructor(address yourBaseAddr) WarGameUnit(yourBaseAddr) public {}
+    constructor(address yourBaseAddr) WarGameUnit(yourBaseAddr) public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+    }
     
   
 }
